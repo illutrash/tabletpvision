d97a4f54bcccfbb4613c7ddc29fa40af8607449b
Christopher <doering.christopher1808@gmail.com>
2020-03-24 21:11:20 +0100

    Initial Commit

---
+++ commercial_painting/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommercialPaintingConfig(AppConfig):
+    name = 'commercial_painting'
---
+++ tabletopvision/settings/development.py
@@ -0,0 +1,43 @@
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True
+        },
+        'general': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'alerts': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
+
